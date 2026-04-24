c5191613669ec5da4402e7eec7ece5b25201217b
binmite <tomasovartem039@gmail.com>
2026-04-24 16:57:17 +0300

    add Monster logic

--- maze-game c#/Monster.cs
+++ maze-game c#/Monster.cs
@@ -8,19 +8,77 @@ namespace maze_game_c_
 {
     internal class Monster
     {
-        public int x;
-        public int y;
-        public int difficulty;
-        public int currentQuestion;
-        public int answer;
-        public bool isDefeated;
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Difficulty { get; set; }
+        public string CurrentQuestion { get; set; }
+        public int Answer { get; set; }
+        public bool IsDefeated { get; set; }
 
-        //public static Monster GenerateQuestion(int difficulty)
-        //{
-        //    if (difficulty == 1)
-        //    {
+        private static readonly Random _random = new Random();
 
-        //    }
-        //}
+        public Monster(int x, int y, int difficulty)
+        {
+            X = x;
+            Y = y;
+            Difficulty = difficulty;
+            IsDefeated = false;
+            GenerateQuestion();
+        }
+
+        public bool CheckAnswer(int playerAnswer)
+        {
+            if (playerAnswer == Answer)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void GenerateQuestion()
+        {
+            if (Difficulty == 1)
+            {
+                var questions = new List<(string, int)>
+                {
+                    ("5 + 3 = ?", 8),
+                    ("10 - 4 = ?", 6),
+                    ("2 * 3 = ?", 6)
+                };
+
+                var selected = questions[_random.Next(questions.Count)];
+                CurrentQuestion = selected.Item1;
+                Answer = selected.Item2;
+            }
+
+            else if (Difficulty == 2)
+            {
+                var questions = new List<(string, int)>
+                {
+                    ("15 + 27 = ?", 42),
+                    ("50 - 18 = ?", 32),
+                    ("8 * 7 = ?", 56)
+                };
+
+                var selected = questions[_random.Next(questions.Count)];
+                CurrentQuestion = selected.Item1;
+                Answer = selected.Item2;
+            }
+
+            else if (Difficulty == 3)
+            {
+                var questions = new List<(string, int)>
+                {
+                    ("25 * 4 - 30 = ?", 70),
+                    ("144 / 12 + 7 = ?", 19),
+                    ("√169 = ?", 13)
+                };
+
+                var selected = questions[_random.Next(questions.Count)];
+                CurrentQuestion = selected.Item1;
+                Answer = selected.Item2;
+            }
+        }
     }
 }

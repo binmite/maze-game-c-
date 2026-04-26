746e3a0069bc364168474f832289fdb453db5812
binmite <tomasovartem039@gmail.com>
2026-04-26 03:23:36 +0300

    fix cursor visibility

--- maze-game c#/Map.cs
+++ maze-game c#/Map.cs
@@ -64,6 +64,7 @@
 
         public static void MapRendering(char[,] grid)
         {
+            Console.CursorVisible = false;
             for (int i = 0; i < grid.GetLength(0); i++)
             {
                 for (int j = 0; j < grid.GetLength(1); j++)
